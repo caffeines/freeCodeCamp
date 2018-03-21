911513fde48a1051d795290630d55e2f81c3b4cf
caffeines <me.caffeines@gmail.com>
2018-03-21 14:41:51 +0600

    cool

---
+++ JavaScript/Declare String Variables.js
@@ -0,0 +1,9 @@
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Sadat";
+var myLastName = "Sayem";
+firstName = myFirstName;
+lastName = myLastName;
